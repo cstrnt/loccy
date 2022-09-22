e4740bfe089737e6c4ff8069dfc948cd6fa18170
Tim Raderschad <tim.raderschad@gmail.com>
2022-09-22 11:06:27 +0200

    fix(cli): branch name not working in monorepo

--- packages/cli/src/helpers.ts
+++ packages/cli/src/helpers.ts
@@ -2,7 +2,7 @@ import { exec } from "child_process";
 
 const getBranch = () =>
   new Promise<string>((resolve, reject) => {
-    return exec("git rev-parse --abbrev-ref HEAD", (err, stdout, stderr) => {
+    return exec("git branch --show-current", (err, stdout, stderr) => {
       if (err) reject(`getBranch Error: ${err}`);
       else if (typeof stdout === "string") resolve(stdout.trim());
     });
