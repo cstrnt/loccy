61952fe55a4467b12c62ebe0bbded89f70172a37
Tim Raderschad <tim.raderschad@gmail.com>
2022-11-24 13:05:25 +0100

    feat: update docs

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,6 +1,10 @@
 import { AppProps } from "next/app";
 import "nextra-theme-docs/style.css";
 
+import Prism from "prism-react-renderer/prism";
+(typeof global !== "undefined" ? global : window).Prism = Prism;
+require("prismjs/components/prism-javascript");
+
 export default function Nextra({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
